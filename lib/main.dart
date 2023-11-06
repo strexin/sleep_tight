6bf6710b2fbe2c8d338fd8e1a04618f7c147cb8e
Strexin <59173171+strexin@users.noreply.github.com>
2023-11-06 10:52:37 +0700

    Update to hide top and bottom bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:sleep_tight/login_page.dart';
 
 void main() {
   runApp(const MainApp());
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
 }
 
 class MainApp extends StatelessWidget {
